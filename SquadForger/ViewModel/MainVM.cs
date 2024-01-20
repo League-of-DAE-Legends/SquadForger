6c46d1445a9d47a8686b090cfb696ff6103154a0
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2024-01-20 19:58:26 +0100

    Bound Main VM to View

---
+++ SquadForger/ViewModel/MainVM.cs
@@ -0,0 +1,17 @@
+using System.Diagnostics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SquadForger.ViewModel
+{
+    public class MainVM : ObservableObject
+    {
+        public string WindowTitle { get; private set; } = $"Squad Forger v0.0.1";
+        public RelayCommand OpenGithubRepoCommand { get; private set; }
+
+        private void OpenGithubRepo()
+        {
+            Process.Start(new ProcessStartInfo("https://github.com/League-of-DAE-Legends/SquadForger"));
+        }
+    }
+}
