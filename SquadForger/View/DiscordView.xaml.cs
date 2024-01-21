7c1b9b8a37555f24337dd328c2995ccdee150530
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2024-01-21 14:02:38 +0100

    Setup initial post to discord view & VM

---
+++ SquadForger/View/DiscordView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace SquadForger.View
+{
+	/// <summary>
+	/// Interaction logic for DiscordView.xaml
+	/// </summary>
+	public partial class DiscordView : Page
+	{
+		public DiscordView()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ SquadForger/ViewModel/DiscordVM.cs
@@ -0,0 +1,47 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Text;
+using Discord.Webhook;
+using System.Resources;
+using CommunityToolkit.Mvvm.Input;
+
+namespace SquadForger.ViewModel
+{
+	public class DiscordVM : ObservableObject
+	{
+		private string _textToSend = "<empty>";
+		public string TextToSend
+		{
+			get => _textToSend;
+			private set { _textToSend = value; OnPropertyChanged(nameof(TextToSend)); }
+		}
+		public RelayCommand PostToDiscordCommand { get; private set; }
+
+		public DiscordVM()
+		{
+			PostToDiscordCommand = new RelayCommand(PostToDiscord);
+			TextToSend = GetLongTestText();
+		}
+
+		private async void PostToDiscord()
+		{
+			if (TextToSend.Equals("<empty>"))
+			{
+				return;
+			}
+			
+			string webhooklink = "Extract from PrivateData.config";
+			DiscordWebhookClient webhook = new DiscordWebhookClient(webhooklink);
+			await webhook.SendMessageAsync(TextToSend);
+		}
+
+		private static string GetLongTestText(int lines = 30)
+		{
+			StringBuilder sb = new StringBuilder();
+			for (int i = 0; i < lines; ++i)
+			{
+				sb.AppendLine("This is a very long sentence just to fill some space. Lorem ispum or something I don't know.");
+			}
+			return sb.ToString();
+		}
+	}
+}
