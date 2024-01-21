3660c67766ba62bb40a977edec77857a048cf2e4
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2024-01-21 23:02:49 +0100

    Adding teams from UI
    When we press on Add, internatlly the teams are added, but the UI doesn't update

--- SquadForger/ViewModel/SquadVM.cs
+++ SquadForger/ViewModel/SquadVM.cs
@@ -1,23 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
+using SquadForger.Model;
 using SquadForger.Repository;
 
 namespace SquadForger.ViewModel
 {
     public class SquadVM : ObservableObject
     {
-        public RelayCommand SelectFileCommand { get; private set; }
+        public List<Team> Teams { get; set; } = new List<Team>();
+        
+        public RelayCommand SelectFileCommand { get; set; }
+        public RelayCommand AddTeamsCommand { get; set; }
         private ITeamNamesRepository TeamNamesRepository { get; set; } = new CsvChallongeParser();
+        
+        public string TeamsInput { get; set; }
 
         public SquadVM()
         {
             SelectFileCommand = new RelayCommand(ReadTeamsFromCsv);
+            AddTeamsCommand = new RelayCommand(AddTeams);
+            TeamsInput = "Enter team names, separated by commas";
         }
 
         private void ReadTeamsFromCsv()
         {
-            var teams = TeamNamesRepository.GetTeams();
+            Teams.AddRange(TeamNamesRepository.GetTeams());
+        }
+
+        private void AddTeams()
+        {
+            // Split the input by commas and remove empty entries
+            List<string> teamNames = TeamsInput.Split(',').Select(s => s.Trim()).Where(s => !string.IsNullOrWhiteSpace(s)).ToList();
+
+            // Create Team objects from team names
+            foreach (string teamName in teamNames)
+            {
+                Teams.Add(new Team { TeamName = teamName });
+            }
+            OnPropertyChanged(nameof(Teams));
         }
     }
 }
