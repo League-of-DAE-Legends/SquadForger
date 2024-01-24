1ab715516e32f1729dff5eef4d0238767c289396
Wardergrip <42802496+Wardergrip@users.noreply.github.com>
2024-01-24 17:51:12 +0100

    Added struct to keep league version

---
+++ SquadForger/Model/LeagueVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquadForger.Model
+{
+	public struct LeagueVersion
+	{
+		public LeagueVersion(int season, int patchNumber, int subpatchNumber = 1)
+		{
+			Season = season;
+			PatchNumber = patchNumber;
+			SubpatchNumber = subpatchNumber;
+		}
+
+		public int Season;
+		public int PatchNumber;
+		public int SubpatchNumber;
+	}
+}
