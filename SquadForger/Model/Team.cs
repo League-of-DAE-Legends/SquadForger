d9aab0bbf2ed3b322150e92d99bd45929cb2fd29
hovo-hakobyan <88614889+hovo-hakobyan@users.noreply.github.com>
2024-02-01 11:30:15 +0100

    Team info is formatted in a different way
    Now there is 5 champions on 1 line, then a new line follows

--- SquadForger/Model/Team.cs
+++ SquadForger/Model/Team.cs
@@ -13,10 +13,21 @@ namespace SquadForger.Model
             var stringBuilder = new StringBuilder();
             stringBuilder.AppendLine($"# **{TeamName}:**");
 
+            int count = 0; // Initialize a counter for the champions
+
             foreach (var champion in ChampionNames)
             {
-                stringBuilder.AppendLine($"- {champion}");
+                stringBuilder.Append($"{champion}, ");
+                count++; // Increment the counter for each champion
+
+                // Check if the count is a multiple of 5
+                if (count % 5 == 0)
+                {
+                    stringBuilder.AppendLine(); // Append an additional new line after every 5 champions
+                }
             }
+            
+            
 
             return stringBuilder.ToString();
         }
